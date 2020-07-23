4a6cdf541d433e3c962d4e95e57a635d694f5c2d
Yahor H <yegorich_555@mail.ru>
2020-07-23 15:51:55 +0300

    Styles. Remove useless

--- src/components/homeView.tsx
+++ src/components/homeView.tsx
@@ -35,24 +35,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lighter,
   },
   body: {
+    flex: 1,
     backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
